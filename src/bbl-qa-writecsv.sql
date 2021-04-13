b1c2ee61679861a9e738316bb38a9de5497f086d
Matt Schell <mschell@doitt.nyc.gov>
2021-04-13 11:05:16 -0400

    maximizes bbls in bbl-dab-history

--- src/bbl-qa-writecsv.sql
+++ src/bbl-qa-writecsv.sql
@@ -5,5 +5,5 @@ SET TERMOUT OFF
 SET PAGESIZE 0
 SPOOL bbl-qa.csv
 select 'DOITT_ID, BASE_BBL' from dual;
-select doitt_id, base_bbl from bbl_qa order by doitt_id;
+select doitt_id || ',' || base_bbl from bbl_qa order by doitt_id;
 SPOOL OFF
