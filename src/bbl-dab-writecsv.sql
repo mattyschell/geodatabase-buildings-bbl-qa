92d63b0e3c93ac0b2348b9844cdea6d1a8f17e30
Matt Schell <mschell@doitt.nyc.gov>
2021-04-15 14:46:02 -0400

    replaces dab-history lot action with words

--- src/bbl-dab-writecsv.sql
+++ src/bbl-dab-writecsv.sql
@@ -7,12 +7,28 @@ set LINESIZE 9999
 set TRIMSPOOL ON
 @src/bbl-dab-load.sql
 SPOOL bbl-dab-history.csv
-select 'BBL, CHANGE_DATE, WIZARD_NAME, LOT_ACTION, AUTH_FOR_CHANGE' from dual;
-select b.bbl || ',' ||
-       to_char(a.change_date, 'YYYY-MM-DD') || ',' ||
-       a.wizard_name || ',' ||
-       b.lot_action || ',' ||
-       substr(replace(replace(replace(a.auth_for_change,chr(10),' '),chr(13),' '),',',' '),1,128) as auth_for_change 
+select '"BBL", "CHANGE_DATE", "WIZARD_NAME", "LOT_ACTION", "AUTH_FOR_CHANGE"' from dual;
+select 
+    b.bbl || ',' ||
+    to_char(a.change_date, 'YYYY-MM-DD') || ',' ||
+    a.wizard_name || ',' ||
+    case b.lot_action 
+        when 'A' then '"Altered"'
+        when 'B' then '"Block Number Changed"'
+        when 'C' then '"Condo Conversion"'
+        when 'D' then '"Dropped"'
+        when 'L' then '"Lot Number Changed"'
+        when 'M' then '"Condo Unit Merge"'
+        when 'N' then '"New"'
+        when 'P' then '"Condo Unit Apportionment"'
+        when 'T' then '"Condo Termination"'
+        when 'U' then '"Condo Number Change (Update)"'
+        when 'X' then '"Number Change (Old Number no longer exists)"'
+        when 'Y' then '"Effective Tax Year Change"'        
+        else null end || ',' ||
+    '"' || substr(replace(replace(replace(replace(a.auth_for_change,chr(10),' '),chr(13),' '),',',' '),'"','""')
+                  ,1,128) 
+    || '"'
 from 
     dof_taxmap.dab_wizard_transaction a
 join
