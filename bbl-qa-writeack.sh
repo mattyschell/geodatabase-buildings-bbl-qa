027f2ea4a0c4d16965e29d20e1b8f3e7ae24ed5d
Matt Schell <mschell@doitt.nyc.gov>
2021-04-15 16:23:20 -0400

    improves shell script output formatting

--- bbl-qa-writeack.sh
+++ bbl-qa-writeack.sh
@@ -2,11 +2,10 @@
 rm src/bbl-qa-ack.sql
 # strip any windows line separators
 dos2unix bbl-qa.csv
-# ensure an empty line at the end of the csv
-echo >> bbl-qa-ack.csv
-cat bbl-qa-ack.csv | while read y
-do
-echo "insert into bbl_qa_ack values($y); " >> src/bbl-qa-ack.sql
-done
+# this works regardless of newlines at eof
+# two newlines eof still a problem 
+while IFS= read -r LINE || [[ -n "$LINE" ]]; do
+    echo "insert into bbl_qa_ack values($LINE); " | tr -d '\r' >> src/bbl-qa-ack.sql
+done < bbl-qa-ack.csv
 echo "commit;" >> src/bbl-qa-ack.sql 
 echo "finished writing src/bbl-qa-ack.sql "
