90f469c056232e3f7341560939d23fd1d5d75968
Matt Schell <mschell@doitt.nyc.gov>
2022-07-18 11:24:48 -0400

    corrects logging in sample batch file

--- buildings-bbl-qa-sample.bat
+++ buildings-bbl-qa-sample.bat
@@ -16,21 +16,21 @@ set BBLQA=C:\gis\geodatabase-buildings-bbl-qa\
 set BATLOG=%TARGETLOGDIR%buildings-bbl-qa.log
 set PROPY=c:\Progra~1\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe
 set TAXPOLYS=TAX_LOT_POLYGON
-echo starting import of tax lots to %DBNAME% on %date% at %time% > %BATLOG%
+echo starting import of tax lots to %DBNAME% on %date% at %time% >> %BATLOG%
 %PROPY% %BBLQA%delete.py %TAXPOLYS% && (
-  echo. deleted target %TAXPOLYS% on %date% at %time% >> %BATLOG%
+  echo. >> %BATLOG% echo deleted target %TAXPOLYS% on %date% at %time% >> %BATLOG%
 ) || (
   %PROPY% %BBLQA%notify.py "Failed to delete %TAXPOLYS% on %SDEFILE%" %NOTIFY% && EXIT /B 1
 )  
 %PROPY% %BBLQA%import.py %TAXPOLYS% %SOURCEFC% && (
-  echo. >> %BATLOG% echo imported target %TAXPOLYS% on %date% at %time% >> %BATLOG%
+  echo. >> %BATLOG% echo >> %BATLOG% echo imported target %TAXPOLYS% on %date% at %time% >> %BATLOG%
 ) || (
   %PROPY% %BUILDINGS%notify.py "Failed to import %TAXPOLYS% on %SDEFILE%" %NOTIFY% && EXIT /B 1
 ) 
-echo. >> starting qa of building bbls in %DBNAME% on %date% at %time% >> %BATLOG%
+echo. >> %BATLOG% echo starting qa of building bbls in %DBNAME% on %date% at %time% >> %BATLOG%
 sqlplus -s -l BLDG/%DBPASS%@%DBNAME% @run.sql && (
   %PROPY% %BBLQA%notify.py "Completed buildings-bbl-qa in %DBNAME%" %NOTIFY% 
 ) || (
   %PROPY% %BBLQA%notify.py "Failed to execute buildings-bbl-qa in %DBNAME%" %NOTIFY% && EXIT /B 1
 ) 
-echo. >> geodatabase-buildings-bbl-qa sent output to %NOTIFY% >> %BATLOG% 
+echo. >> %BATLOG% echo geodatabase-buildings-bbl-qa sent output to %NOTIFY% >> %BATLOG% 
